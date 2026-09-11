e24f890f8f9e94e25d669088f264121393d49d91
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-11 09:12:42 +0800

    Initialize scimatic_mobile with metadata and imports

---
+++ scimatic_mobile/__init__.py
@@ -0,0 +1,12 @@
+from . import convenient_utils
+
+'''
+SciMatic
+A lightweight Python library for
+mathematics, science calculations,
+and experimental functions.
+'''
+
+__version__ = '1.32.1'
+__author__ = 'Th3Curs3dOn3'
+__license__ = 'MIT'
