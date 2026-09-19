3f1dd972e6b14c08700b367fcd7bd57431ab254c
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-19 12:47:08 +0800

    Update circuits.py

--- scimatic_mobile/c_engineering/circuits.py
+++ scimatic_mobile/c_engineering/circuits.py
@@ -3,18 +3,23 @@ from scimatic.c_engineering.logic_gates import XOR, AND, OR
 def half_adder(
 	a: bool | int,
 	b: bool | int,
-	binary: bool = False
+	binary: bool = False,
+	print_result: bool = False
 ) -> tuple[bool | int, bool | int]:
 	SUM = XOR(a, b, binary=binary)
 	COUT = AND(a, b, binary=binary)
 	
+	if print_result:
+		print(f'SUM: {SUM}')
+		print(f'COUT: {COUT}')
 	return SUM, COUT
 
 def full_adder(
 	x: bool | int,
 	y: bool | int,
 	z: bool | int,
-	binary: bool = False
+	binary: bool = False,
+	print_result: bool = False
 ) -> tuple[bool | int, bool | int]:
 	
 	#First step produces 4 variables a, b, c, d
@@ -27,7 +32,10 @@ def full_adder(
 	SUM = XOR(a, x, binary = binary)
 	e = OR(b, c, binary=binary)
 	
-	#Third step producesthe carry-out(COUT)
+	#Third step produces the carry-out(COUT)
 	COUT = OR(d, e, binary=binary)
 	
+	if print_result:
+		print(f'SUM: {SUM}')
+		print(f'COUT: {COUT}')
 	return SUM, COUT
