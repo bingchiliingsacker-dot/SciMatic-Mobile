b60f5533e2193a3083254cf88ed820a02cf9e56f
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-19 12:47:29 +0800

    Update logic_gates.py

--- scimatic_mobile/c_engineering/logic_gates.py
+++ scimatic_mobile/c_engineering/logic_gates.py
@@ -1,68 +1,116 @@
 def AND(
-	bool1: bool | int,
-	bool2: bool | int,
-	binary: bool = False
+        bool1: bool | int,
+        bool2: bool | int,
+        binary: bool = False,
+        print_result: bool = False
 ) -> bool | int:
-	
-	result = bool(bool1) and bool(bool2)
-	
-	return int(result) if binary else result
+
+    result = bool(bool1) and bool(bool2)
+
+    output = int(result) if binary else result
+
+    if print_result:
+        print(output)
+
+    return output
+
 
 def NOT(
-	bool1: bool | int,
-	binary: bool = False
+        bool1: bool | int,
+        binary: bool = False,
+        print_result: bool = False
 ) -> bool | int:
-	
-	result = not bool(bool1)
-	
-	return int(result) if binary else result
+
+    result = not bool(bool1)
+
+    output = int(result) if binary else result
+
+    if print_result:
+        print(output)
+
+    return output
+
 
 def NAND(
-	bool1: bool | int,
-	bool2: bool | int,
-	binary: bool = False
+        bool1: bool | int,
+        bool2: bool | int,
+        binary: bool = False,
+        print_result: bool = False
 ) -> bool | int:
-	
-	result = NOT(AND(bool1, bool2))
-	
-	return int(result) if binary else result
+
+    result = NOT(AND(bool1, bool2))
+
+    output = int(result) if binary else result
+
+    if print_result:
+        print(output)
+
+    return output
+
 
 def OR(
-	bool1: bool | int,
-	bool2: bool | int,
-	binary: bool = False
+        bool1: bool | int,
+        bool2: bool | int,
+        binary: bool = False,
+        print_result: bool = False
 ) -> bool | int:
-	
-	result = bool(bool1) or bool(bool2)
-	
-	return int(result) if binary else result
+
+    result = bool(bool1) or bool(bool2)
+
+    output = int(result) if binary else result
+
+    if print_result:
+        print(output)
+
+    return output
+
 
 def XOR(
-	bool1: bool | int,
-	bool2: bool | int,
-	binary: bool = False
+        bool1: bool | int,
+        bool2: bool | int,
+        binary: bool = False,
+        print_result: bool = False
 ) -> bool | int:
-	
-	result = bool(bool1) != bool(bool2)
-	
-	return int(result) if binary else result
-	
+
+    result = bool(bool1) != bool(bool2)
+
+    output = int(result) if binary else result
+
+    if print_result:
+        print(output)
+
+    return output
+
+
 def NOR(
-	bool1: bool | int,
-	bool2: bool | int,
-	binary: bool = False
+        bool1: bool | int,
+        bool2: bool | int,
+        binary: bool = False,
+        print_result: bool = False
 ) -> bool | int:
-	
-	result = NOT(OR(bool1, bool2))
-	
-	return int(result) if binary else result
+
+    result = NOT(OR(bool1, bool2))
+
+    output = int(result) if binary else result
+
+    if print_result:
+        print(output)
+
+    return output
+
 
 def XNOR(
-	bool1: bool | int,
-	bool2: bool | int,
-	binary: bool = False
+        bool1: bool | int,
+        bool2: bool | int,
+        binary: bool = False,
+        print_result: bool = False
 ) -> bool | int:
-	
-	result = bool(bool1) == bool(bool2)
 
-	return int(result) if binary else result
+    result = bool(bool1) == bool(bool2)
+
+    output = int(result) if binary else result
+
+    if print_result:
+        print(output)
+
+    return output
