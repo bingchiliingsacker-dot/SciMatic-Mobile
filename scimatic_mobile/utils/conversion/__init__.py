633e76c6f7b5af126a57ae4667318d8e80b679fb
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-20 10:27:45 +0800

    Fix import path for measurement module

--- scimatic_mobile/utils/conversion/__init__.py
+++ scimatic_mobile/utils/conversion/__init__.py
@@ -1,4 +1,4 @@
-from .measurement import convert_len, convert_area
+from .measurements import convert_len, convert_area
 from .currency import fresh_convert_curr, cached_convert_curr
 from .data import convert_bits
 from .temperature import convert_temp
