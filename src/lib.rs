9ffa4788b3cd93146c08f36a0931172095a513c2
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-25 18:03:23 +0800

    Refactor flicker module and clean up imports
    Removed unused module imports and retained flicker module functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,8 @@
 mod flicker;
-mod conversion;
-
 use pyo3::prelude::*;
 
 #[pymodule]
 fn flicker_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(flicker::flick, m)?)?;
-    Ok(())
-}
-
-#[pymodule]
-fn rs_converters(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(conversion::convert_binary, m)?)?;
     Ok(())
 }
