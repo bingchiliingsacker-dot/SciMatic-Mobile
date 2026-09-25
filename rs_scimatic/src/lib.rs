18564dc5e9d1e886526701a9d79ab1c4328c4572
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-25 20:59:09 +0800

    Update lib.rs

--- rs_scimatic/src/lib.rs
+++ rs_scimatic/src/lib.rs
@@ -1,1 +1,20 @@
-#ph
+use pyo3::prelude::*;
+
+#[pymodule]
+fn scimatic(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+			 // Wrapping flicker_module
+    let flicker = PyModule::new(py, "flicker")?;
+    flicker.add_function(
+        wrap_pyfunction!(flicker_module::flick, &flicker)?
+    )?;
+    m.add_submodule(&flicker)?;
+    
+    // Wrapping rs_converters
+    let conversion = PyModule::new(py, "conversion")?;
+    conversion.add_function(
+        wrap_pyfunction!(rs_converters::convert_binary, &conversion)?
+    )?;
+    m.add_submodule(&conversion)?;
+
+    Ok(())
+}
