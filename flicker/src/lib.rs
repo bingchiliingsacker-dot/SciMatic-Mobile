7169e5a2bcf22776aeed5a8510a27c3da0a6f765
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-26 11:06:15 +0800

    Delete flicker directory

--- flicker/src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod flicker;
-use pyo3::prelude::*;
-
-#[pymodule]
-fn flicker_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(flicker::flick, m)?)?;
-    Ok(())
-}
