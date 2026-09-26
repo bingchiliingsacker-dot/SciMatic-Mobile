d11741a2df94d4b6acc4a089d77e4bf82fe4679a
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-26 11:05:18 +0800

    Delete rs_converters directory

--- rs_converters/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod conversion;
-
-use pyo3::prelude::*;
-
-#[pymodule]
-fn rs_converters(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(conversion::convert_binary, m)?)?;
-    Ok(())
-}
