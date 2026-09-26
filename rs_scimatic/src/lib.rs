f86cbfbae82431c041ecac3a40d16c73042b1717
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-26 11:07:53 +0800

    Refactor module wrapping for flicker and conversion

--- rs_scimatic/src/lib.rs
+++ rs_scimatic/src/lib.rs
@@ -1,22 +1,25 @@
+mod flicker;
+mod conversion;
+
 use pyo3::prelude::*;
-use flicker_module;
-use rs_converters;
 
 #[pymodule]
 fn scimatic(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
-			 // Wrapping flicker_module
-    let flicker = PyModule::new(py, "flicker")?;
-    flicker.add_function(
-        wrap_pyfunction!(flicker_module::flick, &flicker)?
-    )?;
-    m.add_submodule(&flicker)?;
-    
-    // Wrapping rs_converters
-    let conversion = PyModule::new(py, "conversion")?;
-    conversion.add_function(
-        wrap_pyfunction!(rs_converters::convert_binary, &conversion)?
-    )?;
-    m.add_submodule(&conversion)?;
+	let flicker_module = PyModule::new(py, "flicker")?;
 
-    Ok(())
+	flicker_module.add_function(
+		wrap_pyfunction!(flicker::flick, &flicker_module)?
+	)?;
+
+	m.add_submodule(&flicker_module)?;
+
+	let conversion_module = PyModule::new(py, "conversion")?;
+
+	conversion_module.add_function(
+		wrap_pyfunction!(conversion::convert_binary, &conversion_module)?
+	)?;
+
+	m.add_submodule(&conversion_module)?;
+
+	Ok(())
 }
